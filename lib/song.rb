02838a47e52a7b79adfacc123a4a377ee63afa7a
Seth Wax <sethlwax@gmail.com>
2018-04-10 07:12:56 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ require 'pry'
 require_relative '../config/environment.rb'
 
 class Song
-  extend Memorable
+  extend Memorable, Findable
 
   attr_accessor :name
   attr_reader :artist
@@ -11,10 +11,6 @@ class Song
 
   def initialize
     @@songs << self
-  end
-
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
   end
 
   def artist=(artist)
